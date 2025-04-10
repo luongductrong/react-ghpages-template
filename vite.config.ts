5a8458e41d94d52662a50f27c9cc5f1f2e1f214d
luongductrong <luongductrong2004@gmail.com>
2025-04-10 10:08:04 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    host: '0.0.0.0',
+  },
 })
